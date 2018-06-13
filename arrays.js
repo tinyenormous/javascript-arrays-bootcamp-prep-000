1917fa34d79b066a7c3d13d854dbc35ea4534420
rossdaly@gmail.com daly <rossdaly@gmail.com>
2018-06-13 15:47:42 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,34 @@
+var chocolateBars=[ 'snickers','hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array,element) {
+  var returnArray= [element,...array]
+  return returnArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array,element) {
+  var returnArray= [...array,element]
+  return returnArray
+}
+
+function destructivelyAddElementToEndOfArray(array,element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array,index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return unshift.array
+  
+}
